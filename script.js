913bb7eafa073c3bb74555354249c75bfc6e1636
VanGuardf <alisaadati18000@gmail.com>
2024-01-13 21:18:32 +0330

    Add Interval to clock

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let minute = 0;
 let hour = 0;
 let day = new Date();
 
+setInterval(function () {
+  let date = new Date();
+  seccond = date.getSeconds() * 6;
+  minute = date.getMinutes() * 6;
+  hour = date.getHours() * 30 + Math.round(minute / 12);
+  document.getElementById("seccond-hand").style.transform =
+    "rotate(" + seccond + "deg)";
+  document.getElementById("minute-hand").style.transform =
+    "rotate(" + minute + "deg)";
+  document.getElementById("hour-hand").style.transform =
+    "rotate(" + hour + "deg)";
+}),
+  1000;
